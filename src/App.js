8759e6f04eee6ee1b537f1a8336c33fcc957ba63
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-06-21 15:41:30 +0300

    Form section finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Header from './components/Header'
+import Form from './components/Form'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Form/>
+
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './Form.css'
+function Form() {
+  return(
+    <main>
+            <form className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
+                    className="form--button"
+                >
+                    Get a new meme image 🖼
+                </button>
+            </form>
+        </main>
+  )
+}
+
+export default Form
